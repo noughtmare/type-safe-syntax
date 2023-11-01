df96807e27a78250f2f86f0912c0e8264e510f32
Jaro Reinders <jaro.reinders@gmail.com>
2023-11-01 14:09:11 +0100

    Clean up ClosConv

--- ClosConv.hs
+++ ClosConv.hs
@@ -5,6 +5,8 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeAbstractions #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 
 -- Closure conversion
 
@@ -19,16 +21,19 @@ import Data.Type.Equality
 import Data.Functor.Identity
 import Data.Kind
 
+
+-- DEFINITIONS
+
+
 data Var env a where
   VZ :: Var (a : env) a
   VS :: Var env a -> Var (x : env) a
 
 instance Show (Var env a) where
-  show v = show (varToInt v)
-
-varToInt :: Var xs a -> Int
-varToInt VZ = 0
-varToInt (VS v) = 1 + varToInt v
+  show v0 = show (varToInt v0) where
+    varToInt :: Var xs a -> Int
+    varToInt VZ = 0
+    varToInt (VS v) = 1 + varToInt v
 
 data DB env a where
   Var :: Var env a -> DB env a
@@ -37,10 +42,28 @@ data DB env a where
 
 deriving instance Show (DB env a)
 
+
+-- PART I - FREE VARIABLE ANALYSIS
+
+
 data Sel env env' where
-  Keep :: Sel env env' -> Sel (x : env) (x : env')
-  Drop :: Sel env env' -> Sel (x : env) env'
-  Done :: Sel env '[]
+  Keep :: (KnownList env, KnownList env') => Sel env env' -> Sel (x : env) (x : env')
+  Drop :: (KnownList env, KnownList env') => Sel env env' -> Sel (x : env) env'
+  Done :: Sel '[] '[]
+
+-- -- Normalising keep
+-- pattern Keep :: forall env env'. forall x env'' env'''. (env' ~ (x : env'''), env ~ (x : env'')) => Sel env'' env''' -> Sel env env'
+-- pattern Keep s <- Keep' s where
+--   Keep KeepAll = KeepAll
+--   Keep s = Keep' s
+-- 
+-- -- Normalising drop
+-- pattern Drop :: forall env env'. forall x env''. (env ~ (x : env'')) => Sel env'' env' -> Sel env env'
+-- pattern Drop s <- Drop' s where
+--   Drop DropAll = DropAll
+--   Drop s = Drop' s
+-- 
+-- {-# COMPLETE Keep, Drop, DropAll, KeepAll #-}
 
 deriving instance Show (Sel env env')
 
@@ -51,122 +74,90 @@ data DB' env a where
 
 deriving instance Show (DB' env a)
 
-varSel :: Var env a -> Sel env '[a]
-varSel VZ = Keep Done
-varSel (VS v) = Drop (varSel v)
+type KnownList :: forall {k}. [k] -> Constraint 
+class KnownList xs where
+  listVal :: HList Proxy xs
 
-data ClosResult pre env a where
-  ClosResult :: HList Proxy env' -> Sel env env' -> DB' (pre ++ env') a -> ClosResult pre env a
+instance KnownList '[] where
+  listVal = HNil
+instance KnownList xs => KnownList (x : xs) where
+  listVal = HCons Proxy listVal
 
-deriving instance Show (ClosResult pre env a)
+dropHList :: HList Proxy env -> Sel env '[]
+dropHList HNil = Done
+dropHList (HCons _ xs) = Drop (dropHList xs)
+
+keepAll :: KnownList env => Sel env env
+keepAll = go listVal where
+  go :: HList Proxy env -> Sel env env
+  go HNil = Done
+  go (HCons _ xs) = Keep (go xs)
+
+varSel :: forall env a. KnownList env => Var env a -> Sel env '[a]
+varSel = go (listVal @env) where
+  go :: HList Proxy env' -> Var env' a -> Sel env' '[a]
+  go (HCons _ h) VZ = Keep (dropHList h) where
+  go (HCons _ h) (VS v) = Drop (go h v)
+  go HNil v = case v of {}
+
+data ClosResult env a where
+  ClosResult :: Sel env env' -> DB' env' a -> ClosResult env a
+
+deriving instance Show (ClosResult env a)
 
 data HList f xs where
-  HCons :: f x -> HList f xs -> HList f (x : xs)
+  HCons :: KnownList xs => f x -> HList f xs -> HList f (x : xs)
   HNil :: HList f '[]
 
 deriving instance (forall x. Show (f x)) => Show (HList f xs)
 
-type (++) :: [k] -> [k] -> [k]
-type family xs ++ ys where
-  '[] ++ ys = ys
-  (x : xs) ++ ys = x : (xs ++ ys)
+apSel :: Sel env env' -> Var env' a -> Var env a
+apSel Done v = case v of {}
+apSel (Drop s) v = VS (apSel s v)
+apSel (Keep _) VZ = VZ
+apSel (Keep s) (VS v) = VS (apSel s v)
 
-assoc :: HList Proxy xs -> Proxy ys -> Proxy zs -> ((xs ++ ys) ++ zs :~: xs ++ (ys ++ zs))
-assoc HNil Proxy Proxy = Refl
-assoc (HCons Proxy h) p1 p2 = case assoc h p1 p2 of Refl -> Refl
+withKnownSel :: Sel env env' -> ((KnownList env, KnownList env') => Sel env env' -> a) -> a
+withKnownSel Done f = f Done
+withKnownSel s@Keep{} f = f s
+withKnownSel s@Drop{} f = f s
 
-rightId :: HList Proxy xs -> (xs ++ '[] :~: xs)
-rightId HNil = Refl
-rightId (HCons Proxy h) = case rightId h of Refl -> Refl
+composeSel :: Sel env' env'' -> Sel env env' -> Sel env env''
+composeSel Done s = s
+composeSel s1 (Drop s2) = withKnownSel (composeSel s1 s2) Drop
+composeSel (Keep s1) (Keep s2) = Keep (composeSel s1 s2)
+composeSel (Drop s1) (Keep s2) = Drop (composeSel s1 s2)
 
-hAppend :: HList f xs -> HList f ys -> HList f (xs ++ ys)
-hAppend HNil ys = ys
-hAppend (HCons x xs) ys = HCons x (hAppend xs ys)
+weakenBy :: Sel env env' -> DB' env' a -> DB' env a
+weakenBy s (Var' v) = Var' (apSel s v)
+weakenBy s (App' x y) = App' (weakenBy s x) (weakenBy s y)
+weakenBy s (Lam' s' x) = Lam' (composeSel s' s) x
 
-hSnoc :: HList f xs -> f x -> HList f (xs ++ '[x])
-hSnoc h x = hAppend h (HCons x HNil)
-
-weaken' :: DB' env a -> DB' (x : env) a
-weaken' = weakenUnder' HNil (HCons Proxy HNil) Proxy
-
-weakenVar :: HList Proxy pre -> HList Proxy new -> Proxy env -> Var (pre ++ env) a -> Var (pre ++ (new ++ env)) a
-weakenVar HCons {} _ _ VZ = VZ
-weakenVar (HCons Proxy pre) new env (VS x) = VS (weakenVar pre new env x)
-weakenVar HNil HNil _ x = x
-weakenVar HNil (HCons Proxy new) env x = VS (weakenVar HNil new env x)
-
-weakenUnderSel :: HList Proxy pre -> HList Proxy new -> Proxy env -> Sel (pre ++ env) env' -> Sel (pre ++ (new ++ env)) env'
-weakenUnderSel HCons {} _ _ Done = Done
-weakenUnderSel (HCons Proxy pre) new env (Keep s) = Keep (weakenUnderSel pre new env s)
-weakenUnderSel (HCons Proxy pre) new env (Drop s) = Drop (weakenUnderSel pre new env s)
-weakenUnderSel HNil HNil _ x = x
-weakenUnderSel HNil (HCons Proxy new) env x = Drop (weakenUnderSel HNil new env x)
-
-weakenUnder' :: HList Proxy pre -> HList Proxy new -> Proxy env -> DB' (pre ++ env) a -> DB' (pre ++ (new ++ env)) a
-weakenUnder' pre new env (Var' v) = Var' (weakenVar pre new env v)
-weakenUnder' pre new env (App' x y) = App' (weakenUnder' pre new env x) (weakenUnder' pre new env y)
-weakenUnder' pre new env (Lam' s x) = Lam' (minimize (weakenUnderSel pre new env s)) x
-
-minimize :: Sel env env' -> Sel env env'
-minimize (Drop x) =
-  case minimize x of
-    Done -> Done
-    x' -> Drop x'
-minimize (Keep x) = Keep (minimize x)
-minimize Done = Done
-
-idSel :: HList Proxy env -> Sel env env
-idSel HNil = Done
-idSel (HCons Proxy h) = Keep (idSel h)
-
-close :: DB env a -> ClosResult '[] env a
-close (Var v) = ClosResult (HCons Proxy HNil) (varSel v) (Var' VZ)
+close :: KnownList env => DB env a -> ClosResult env a
+close (Var v) = ClosResult (varSel v) (Var' VZ)
 close (Lam x) =
   case close x of
-    ClosResult (HCons Proxy h) (Keep sx) x' -> ClosResult h sx (Lam' (idSel h) x')
-    ClosResult h (Drop sx) x' -> ClosResult h sx (Lam' (idSel h) (weaken' x'))
-    ClosResult HNil Done x' -> ClosResult HNil Done (Lam' Done (weaken' x'))
-close (App x y) = closeApp HNil (close x) (close y)
+    ClosResult (Keep sx) x' -> ClosResult sx (Lam' keepAll x')
+    ClosResult (Drop sx) x' -> ClosResult sx (Lam' keepAll (weakenBy (Drop keepAll) x'))
+close (App x y) = case (close x, close y) of
+  (ClosResult s1 x1, ClosResult s2 x2) ->
+    case mergeSel s1 s2 of
+      MergeResult s3 s1' s2' -> 
+        ClosResult s3 (weakenBy s1' x1 `App'` weakenBy s2' x2)
 
-closeApp :: HList Proxy pre -> ClosResult pre env (a -> b) -> ClosResult pre env a -> ClosResult pre env b
-closeApp pre (ClosResult hx (Drop sx) x) (ClosResult hy (Drop sy) y) =
-  case closeApp pre (ClosResult hx sx x) (ClosResult hy sy y) of
-    ClosResult hz sz z -> ClosResult hz (Drop sz) z
-closeApp pre (ClosResult (HCons @_ @x @xs Proxy hx) (Keep sx) x) (ClosResult (HCons @_ @_ @ys Proxy hy) (Keep sy) y) =
-  case (assoc pre (Proxy @'[x]) (Proxy @xs), assoc pre (Proxy @'[x]) (Proxy @ys)) of
-    (Refl, Refl) ->
-      case closeApp (hSnoc pre (Proxy @x)) (ClosResult hx sx x) (ClosResult hy sy y) of
-        ClosResult @env' hz sz z ->
-          case assoc pre (Proxy @'[x]) (Proxy @env') of
-            Refl -> ClosResult (HCons (Proxy @x) hz) (Keep sz) z
-closeApp pre (ClosResult (HCons @_ @x @xs Proxy hx) (Keep sx) x) (ClosResult @ys hy (Drop sy) y) =
-  case (assoc pre (Proxy @'[x]) (Proxy @xs), assoc pre (Proxy @'[x]) (Proxy @ys)) of
-    (Refl, Refl) ->
-      case closeApp
-        (hSnoc pre (Proxy @x))
-        (ClosResult hx sx x)
-        (ClosResult hy sy (weakenUnder' pre (HCons (Proxy @x) HNil) (Proxy @ys) y)) of
-        ClosResult @env' hz sz z ->
-          case assoc pre (Proxy @'[x]) (Proxy @env') of
-            Refl -> ClosResult (HCons (Proxy @x) hz) (Keep sz) z
-closeApp pre (ClosResult @xs hx (Drop sx) x) (ClosResult (HCons @_ @y @ys Proxy hy) (Keep sy) y) =
-  case (assoc pre (Proxy @'[y]) (Proxy @xs), assoc pre (Proxy @'[y]) (Proxy @ys)) of
-    (Refl, Refl) ->
-      case closeApp
-        (hSnoc pre (Proxy @y))
-        (ClosResult hx sx (weakenUnder' pre (HCons (Proxy @y) HNil) (Proxy @xs) x))
-        (ClosResult hy sy y) of
-        ClosResult @env' hz sz z ->
-          case assoc pre (Proxy @'[y]) (Proxy @env') of
-            Refl -> ClosResult (HCons (Proxy @y) hz) (Keep sz) z
-closeApp pre (ClosResult HNil Done x) (ClosResult hy sy y) =
-  case rightId hy of
-    Refl ->
-      ClosResult hy sy (App' (weakenUnder' pre hy (Proxy @'[]) x) y)
-closeApp pre (ClosResult hx sx x) (ClosResult HNil Done y) =
-  case rightId hx of
-    Refl ->
-      ClosResult hx sx (App' x (weakenUnder' pre hx (Proxy @'[]) y))
+data MergeResult env env1 env2 where
+  MergeResult :: KnownList env3 => Sel env env3 -> Sel env3 env1 -> Sel env3 env2 -> MergeResult env env1 env2
+
+mergeSel :: Sel env env1 -> Sel env env2 -> MergeResult env env1 env2
+mergeSel Done Done = MergeResult Done Done Done
+mergeSel (Drop s1) (Drop s2) = case mergeSel s1 s2 of MergeResult s' w1 w2 -> MergeResult (Drop s') w1 w2
+mergeSel (Keep s1) (Drop s2) = case mergeSel s1 s2 of MergeResult s' w1 w2 -> MergeResult (Keep s') (Keep w1) (Drop w2)
+mergeSel (Drop s1) (Keep s2) = case mergeSel s1 s2 of MergeResult s' w1 w2 -> MergeResult (Keep s') (Drop w1) (Keep w2)
+mergeSel (Keep s1) (Keep s2) = case mergeSel s1 s2 of MergeResult s' w1 w2 -> MergeResult (Keep s') (Keep w1) (Keep w2)
+
+
+-- PART II - LIFTING TO THE TOP LEVEL
+
 
 type FunType :: [Type] -> Type -> Type -> Type
 data FunType ctx a b
@@ -189,9 +180,39 @@ data TopLevel fun a where
 deriving instance Show (TopLevel fun a)
 
 data ConvResult var a where
-  ConvResult :: HList (TopLevel fun) fun -> Fun fun var a -> ConvResult var a
+  ConvResult :: KnownList fun => HList (TopLevel fun) fun -> Fun fun var a -> ConvResult var a
 
 deriving instance Show (ConvResult var a)
+
+type (++) :: [k] -> [k] -> [k]
+type family xs ++ ys where
+  '[] ++ ys = ys
+  (x : xs) ++ ys = x : (xs ++ ys)
+
+assoc :: HList Proxy xs -> Proxy ys -> Proxy zs -> ((xs ++ ys) ++ zs :~: xs ++ (ys ++ zs))
+assoc HNil Proxy Proxy = Refl
+assoc (HCons Proxy h) p1 p2 = case assoc h p1 p2 of Refl -> Refl
+
+rightId :: HList Proxy xs -> (xs ++ '[] :~: xs)
+rightId HNil = Refl
+rightId (HCons Proxy h) = case rightId h of Refl -> Refl
+
+withKnownHList :: HList f xs -> (KnownList xs => HList f xs -> a) -> a
+withKnownHList HNil f = f HNil
+withKnownHList h@HCons{} f = f h
+
+hAppend :: HList f xs -> HList f ys -> HList f (xs ++ ys)
+hAppend HNil ys = ys
+hAppend (HCons x xs) ys = withKnownHList (hAppend xs ys) (HCons x)
+
+hSnoc :: HList f xs -> f x -> HList f (xs ++ '[x])
+hSnoc h x = hAppend h (HCons x HNil)
+
+weakenVar :: HList Proxy pre -> HList Proxy new -> Proxy env -> Var (pre ++ env) a -> Var (pre ++ (new ++ env)) a
+weakenVar HCons {} _ _ VZ = VZ
+weakenVar (HCons Proxy pre) new env (VS x) = VS (weakenVar pre new env x)
+weakenVar HNil HNil _ x = x
+weakenVar HNil (HCons Proxy new) env x = VS (weakenVar HNil new env x)
 
 weakenFun :: HList Proxy pre -> HList Proxy new -> Proxy fun -> Fun (pre ++ fun) var a -> Fun (pre ++ (new ++ fun)) var a
 weakenFun h1 h2 p3 = \case
@@ -244,15 +265,20 @@ conv (App' x y) =
           in
           case (rightId (hmap (const Proxy) hx), rightId (hmap (const Proxy) hy)) of 
             (Refl, Refl) -> 
-              ConvResult 
+              withKnownHList
                 (hAppend
                   (hmap (weakenTopLevel hpx hpy (Proxy @'[])) hx)
                   (hmap (weakenTopLevel HNil hpx (Proxy @funy)) hy)
                 )
-                (weakenFun hpx hpy (Proxy @'[]) x' :$ weakenFun HNil hpx (Proxy @funy) y')
+                (\h' ->
+                  ConvResult  h'
+                    (weakenFun hpx hpy (Proxy @'[]) x' :$ weakenFun HNil hpx (Proxy @funy) y')
+                )
 
 closureConvert :: DB '[] a -> ConvResult (HList Identity '[]) a
-closureConvert x = case close x of ClosResult _ Done y -> conv y
+closureConvert x = 
+  case close x of
+    ClosResult Done y -> conv y
 
 infixl `App`
 
@@ -266,12 +292,12 @@ pred =
 -- ClosResult HNil Done (Lam' Done (Lam' (Keep Done) (Lam' (Keep (Keep Done)) (App' (App' (App' (Var' (VS (VS VZ))) (Lam' (Drop (Keep Done)) (Lam' (Keep (Keep Done)) (App' (Var' VZ) (App' (Var' (VS VZ)) (Var' (VS (VS VZ)))))))) (Lam' (Keep Done) (Var' (VS VZ)))) (Lam' Done (Var' VZ))))))
 
 
-pred' :: DB' '[] ((((a1 -> a2) -> (a2 -> b1) -> b1) -> (a3 -> b2) -> (b3 -> b3) -> b4) -> a1 -> b2 -> b4)
-pred' = Lam' Done (Lam' (Keep Done) (Lam' (Keep (Keep Done))
-  (App' (App' (App' (Var' (VS (VS VZ)))
-                    (Lam' (Drop (Keep Done)) (Lam' (Keep (Keep Done)) (App' (Var' VZ) (App' (Var' (VS VZ)) (Var' (VS (VS VZ))))))))
-              (Lam' (Keep Done) (Var' (VS VZ))))
-        (Lam' Done (Var' VZ)))))
+-- pred' :: DB' '[] ((((a1 -> a2) -> (a2 -> b1) -> b1) -> (a3 -> b2) -> (b3 -> b3) -> b4) -> a1 -> b2 -> b4)
+-- pred' = Lam' Done (Lam' (Keep Done) (Lam' (Keep (Keep Done))
+--   (App' (App' (App' (Var' (VS (VS VZ)))
+--                     (Lam' (Drop (Keep Done)) (Lam' (Keep (Keep Done)) (App' (Var' VZ) (App' (Var' (VS VZ)) (Var' (VS (VS VZ))))))))
+--               (Lam' (Keep Done) (Var' (VS VZ))))
+--         (Lam' Done (Var' VZ)))))
 
 -- Without Sel minimization:
 --
